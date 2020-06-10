bb29ae6eab69a9e48a102490a254cc7c605f2924
L-Lisa <lisaojeland@gmail.com>
2020-06-10 19:10:19 +0200

    babel update

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
+import data from "./data.json"
 
 // Defines the port the app will run on. Defaults to 8080, but can be 
 // overridden when starting the server. For example:
@@ -17,6 +18,42 @@ app.use(bodyParser.json())
 app.get('/', (req, res) => {
   res.send('Hello happy world')
 })
+
+app.get("/avocados", (req, res) => {
+  const page = req.query.page ?? 0
+  const pageSize = req.query.pageSize ?? 20
+  const startIndex = page * pageSize
+  const endIndex = startIndex + +pageSize
+
+  const salesForPage = data.slice(startIndex, endIndex)
+
+  const returnObject = {
+    pageSize: pageSize,
+    page: page,
+    maxPages: parseInt(data.length / pageSize),
+    results: salesForPage,
+  }
+
+  res.send(returnObject)
+})
+
+
+app.get("/avocados/:id", (req, res) => {
+  const { id } = req.params //updates to take path variable find "avocados/id"
+  /* console.log(`the req.params are: ${JSON.stringify(req.params)}`) */
+  /* console.log(id) */
+  const findById = data.find( // find the string?
+    (avocado) => avocado.id == id)
+  /*  console.log(findById) */
+  res.send(findById)
+})
+
+/* sort on price functions...
+app.get("/avocados/:averagePrice",(req,res) => {
+  const averagePrice = req.params.averagePrice
+  res.json(averagePrice)
+}) 
+ */
 
 // Start the server
 app.listen(port, () => {
